614bc22908218ac4c3c207ab066cd168c4fbc3ee
Francesco95it <francesco.spagnolo95@gmail.com>
2018-01-23 14:00:34 +0100

    protected routes when typed in url fix

--- src/components/AuthRoute/AuthRoute.js
+++ src/components/AuthRoute/AuthRoute.js
@@ -1,12 +1,32 @@
 import React from 'react'
 import {Redirect} from 'react-router-dom'
 import {connect} from 'react-redux'
+import { sessionService } from 'redux-react-session'
 
 class AuthRoute extends React.Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            sessionLoaded: false
+        }
+    }
+
+    componentWillMount(){
+        sessionService.loadUser()
+        .then(() => {
+            this.setState({
+                sessionLoaded: true
+            });
+        });
+    }
+
     render(){
-        if (this.props.session.authenticated) return this.props.children;
-        return <Redirect to={this.props.back} />
+        if(this.state.sessionLoaded){
+            if (this.props.session.authenticated) return this.props.children;
+            return <Redirect to={this.props.back} />
+        }
+        return null;
     }
 }
 
