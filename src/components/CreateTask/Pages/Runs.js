01fdb8e0e3cbf60bf6774a55cc3b11a77b9ef886
Francesco95it <francesco.spagnolo95@gmail.com>
2018-02-02 15:13:35 +0100

    fix

--- src/components/CreateTask/Pages/Runs.js
+++ src/components/CreateTask/Pages/Runs.js
@@ -161,7 +161,7 @@ export default class Runs extends Component {
 
             })
             promise.then(result => {
-                axios.put('/tasks/runs/'+this.state.runs[index].id, {number: i, imgname: sha256(result), base64: result})
+                axios.put('/tasks/runs/'+this.state.runs[index].id, {imgname: sha256(result), base64: result})
                 .then(res => {
                     let runsCopy = this.state.runs;
                     runsCopy[index].imagesUploading = false;
